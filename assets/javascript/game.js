fe926a8c6c9ecd288589a6458ed2b01faaf0c094
Michael Fox <mikefox17@gmail.com>
2018-11-15 17:35:52 -0500

    added g# display

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
   console.log("ready!");
-});
 
-var wins = 0;
-var losses = 0;
-var playerScore = 0;
-var gameNumber = Math.floor(Math.random() * 101 + 19);
-console.log(gameNumber);
+  var c1 = Math.floor(Math.random() * 11 + 1);
+  console.log(c1);
+  var c2 = Math.floor(Math.random() * 11 + 1);
+  console.log(c2);
+  var c3 = Math.floor(Math.random() * 11 + 1);
+  console.log(c3);
+  var c4 = Math.floor(Math.random() * 11 + 1);
+  console.log(c4);
+  var wins = 0;
+  var losses = 0;
+  var playerScore = 0;
+  var gameNumber = Math.floor(Math.random() * 101 + 19);
+  console.log(gameNumber);
+  $(".gameNumber").html(gameNumber);
+});
