937424f03ff11af126bde74def8325f95c21b7a7
Michael Fox <mikefox17@gmail.com>
2018-11-15 16:16:46 -0500

    css added

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  console.log("ready!");
+});
+
+var wins = 0;
+var losses = 0;
+var playerScore = 0;
+var gameNumber = Math.floor(Math.random() * 101 + 19);
+console.log(gameNumber);
